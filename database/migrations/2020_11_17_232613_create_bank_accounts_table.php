84faa868160508ce876ca2da6aae3ea72c241452
Igor Developer <igor.amaral@iamaral.com.br>
2020-11-17 22:33:30 -0300

    Correçao de campo na migration 'bank_accounts'

--- database/migrations/2020_11_17_232613_create_bank_accounts_table.php
+++ database/migrations/2020_11_17_232613_create_bank_accounts_table.php
@@ -16,7 +16,7 @@ class CreateBankAccountsTable extends Migration
         Schema::create('bank_accounts', function (Blueprint $table) {
             $table->id();
             $table->string('bank_name');
-            $table->string('agency');
+            $table->string('branch');
             $table->string('account')->unique();
             $table->enum('operation', ['corrente', 'poupança']);
             $table->integer('balance')->default(0);
